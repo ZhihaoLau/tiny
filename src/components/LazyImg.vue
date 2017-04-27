aada2b1594e1453e3a9104cafe9e140d25016a3d
Zio Lau <lozio@live.cn>
2017-04-28 02:08:35 +0800

    backup

---
+++ src/components/LazyImg.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="lazy-img">
+    <img src="" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/routes/LazyLoadPage.vue
+++ src/routes/LazyLoadPage.vue
@@ -1,14 +1,47 @@
 <template lang="html">
   <div class="lazy-load-page">
     <p>LazyLoadPage</p>
+    <div class="area" @scroll="onScroll">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consequatur! Nesciunt, ipsa quos. Atque illum impedit et tempora laudantium, placeat, cumque voluptates vel qui adipisci ipsa minima natus saepe, quo.
+      </p>
+      <img src="https://cdn.dribbble.com/users/35877/screenshots/3462035/grace-beziers-dribbble_1x.jpg" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consequatur! Nesciunt, ipsa quos. Atque illum impedit et tempora laudantium, placeat, cumque voluptates vel qui adipisci ipsa minima natus saepe, quo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consequatur! Nesciunt, ipsa quos. Atque illum impedit et tempora laudantium, placeat, cumque voluptates vel qui adipisci ipsa minima natus saepe, quo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, consequatur! Nesciunt, ipsa quos. Atque illum impedit et tempora laudantium, placeat, cumque voluptates vel qui adipisci ipsa minima natus saepe, quo.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+// import LazyImg from '@/components/LazyImg'
 export default {
-  name: 'LazyLoadPage'
+  name: 'LazyLoadPage',
+  methods: {
+    onScroll (e) {
+      console.log(e.target.scrollTop)
+    }
+  }
 }
 </script>
 
 <style lang="css">
+.lazy-load-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.area {
+  height: 40vh;
+  width: 30vh;
+  overflow: scroll;
+  border: 1px solid #ddd;
+}
 </style>
